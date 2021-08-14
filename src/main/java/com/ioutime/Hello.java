abc1f3e3babb0b111e5c59ded436a3c952f2b955
ioutime <xiaoqiusg@gmail.com>
2021-08-15 00:02:36 +0800

    初始化项目

---
+++ src/main/java/com/ioutime/Hello.java
@@ -0,0 +1,30 @@
+package com.ioutime;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+
+/**
+ * @author ioutime
+ * @version 1.0
+ * @date 2021/8/14 22:12
+ */
+
+public class Hello extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+}
